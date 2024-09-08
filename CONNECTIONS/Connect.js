7139eb24c4c78273f81022fd295e9e36736be93d
sudeepti25 <sudeepti2006@gmail.com>
2024-09-08 10:25:12 +0530

    login signup database connection done

--- CONNECTIONS/Connect.js
+++ CONNECTIONS/Connect.js
@@ -1,26 +1,16 @@
-const mysql = require('mysql');
-const express = require('express');
+// Filename: Connect.js
 
-const connection = mysql.createConnection({
-    host: '127.0.0.1',    // Corrected host
-    port: 3306,           // Added port as a separate property
+const mysql = require('mysql');
+
+// Create a pool of connections
+const pool = mysql.createPool({
+    connectionLimit: 10, // Number of connections to maintain in the pool
+    host: '127.0.0.1',
+    port: 3306,
     user: 'root',
     password: 'sudeepti.25',
-    database: "brainbridges"        
-  });
+    database: 'brainbridges'
+});
 
-
-  connection.connect(error => {
-    if (error){
-        console.log("An error has occurred while connecting to the database.");
-        throw error;
-    }
-
-    else
-    {
-        console.log("Database connection established successfully.");
-    }
-
-  });
-
-  module.exports = connection;
+// Export the pool
+module.exports = pool;
