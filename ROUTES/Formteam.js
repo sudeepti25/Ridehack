6c394afe63a79665ebe7edf7d7fc921b5448661e
yash1022 <150256255+yash1022@users.noreply.github.com>
2024-09-09 23:31:10 +0530

    added team fectching functionality along with minor  redirections

---
+++ ROUTES/Formteam.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../CONNECTIONS/Connect');
+
+
+
+router.get('/teams',(req,res)=>{
+
+    const userid= req.session.user.id;
+    const value =req.query.value;
+
+    if(!userid)
+    {
+        res.status(404).send("USER NOT LOGGED IN");
+    }
+
+    const sql = `SELECT name,skills FROM portfolio WHERE skills=?`;
+
+    db.query(sql,[value],(err,result)=>{
+
+        if(err)
+        {
+            res.status(500).send("ERROR FECTHING");
+
+        }
+
+        if(result.length===0)
+        {
+            res.status(404).send("PEOPLE NOT FOUND");
+        }
+
+
+        res.render('showteams',{people:result});
+
+})
+
+    
+
+
+
+})
